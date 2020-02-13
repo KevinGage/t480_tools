909204915efcc80e1862fc36eabff7664ef228f4
KevinGage <kgage@siroonian.com>
2020-02-13 17:03:25 -0500

    added some scripts for toggling power profile

---
+++ power_battery.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Queries or sets the power profile.
+#
+# - If an argument is not provided, the power profile will be queried
+# - Otherwise, that profile will be set, if it is a valid profile
+#
+#USAGE:
+#    system76-power profile [profile]
+#
+#OPTIONS:
+#    -h, --help    Prints help information
+#
+#ARGS:
+#    <profile>    set the power profile [possible values: battery, balanced, performance]
+system76-power profile battery
+system76-power profile
